1fbed3162d7b7b212219f7cf5c87b7a537f8b9cd
Vinaypartap Singh <developervsandhu@gmail.com>
2025-04-07 10:12:08 -0700

    Testing

--- src/components/Greet.jsx
+++ src/components/Greet.jsx
@@ -3,5 +3,10 @@ import React from "react";
 export default function Greet({ name }) {
   if (name) return <h1>Hello, {name}</h1>;
 
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <h1>Hello World</h1>
+      <button>Click Me</button>
+    </>
+  );
 }
--- tests/components/Greet.test.jsx
+++ tests/components/Greet.test.jsx
@@ -1,11 +1,16 @@
-import React from "react";
+import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
-import { describe, it } from "vitest";
+import React from "react";
+import { describe, expect, it } from "vitest";
 import Greet from "../../src/components/Greet";
 
 describe("Greet", () => {
   it("it should render hello world", () => {
     render(<Greet />);
+    expect(screen.getByText("Hello World")).toBeInTheDocument();
+    expect(
+      screen.getByRole("button", { name: "Click Me" })
+    ).toBeInTheDocument();
     screen.debug();
   });
 });
